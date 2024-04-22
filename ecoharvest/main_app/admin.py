80d5f8b1cd6bebbbd010d384a4ef691b6e85fe1f
pdarcy <pdarcy@riseup.net>
2024-04-22 13:35:41 -0400

    add product model

--- ecoharvest/main_app/admin.py
+++ ecoharvest/main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Product, Order, Photo
+from .models import Product, Order, Photo, Pickup
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(Order)
 admin.site.register(Photo)
+admin.ste.register(Pickup)
